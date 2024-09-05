5b0da3cc00559c731068d9f691663cc1ec740d0e
Shayan <karitoswow@gmail.com>
2024-09-04 22:35:08 -0800

    Fixed Rename Model

--- models/Unstuck_Model.php
+++ models/Unstuck_Model.php
@@ -1,6 +1,6 @@
 <?php
 
-class Unstuck_Model extends CI_Model {
+class unstuck_model extends CI_Model {
 
     public function setLocation( $x, $y, $z, $o, $mapId, $characterGuid, $realmConnection ) {
         $realmConnection->query("UPDATE " . table("characters") . " SET " . column("characters", "position_x") . " = ?, " . column("characters", "position_y") . " = ?, " . column("characters", "position_z") . " = ?, " . column("characters", "orientation") . " = ?, " . column("characters", "map") . " = ? WHERE " . column("characters", "guid") . " = ?", [$x, $y, $z, $o, $mapId, $characterGuid]);
